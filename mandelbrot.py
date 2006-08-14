a03fd3ff8b20c4fd75155284d3c87e9e141406f9
xyster <xyster@4caef4ad-2c1a-0410-9467-39dbfd329abf>
2006-08-14 06:50:25 +0000

    Add a somewhat less trivial job, mandelbrot.py. Yet more proof that Python is dog slow? Modify subsystem to track tasks to allow collation and possibly in the future when to recover from bad UVs (no failures allowed yet...). It is currently used also for deciding when to exit. Pass task number into job_get_arg() and job_add_result() so results can be properly collated. Next step is auto-scaling of task chunks to minimize network traffic.

---
+++ mandelbrot.py
@@ -0,0 +1,61 @@
+import Image
+
+x = 0.0
+y = 0.0
+zoom = .01
+width = 320
+height = 200
+dwell =  256 * 16 
+base_x = x - (width / 2) * zoom
+base_y = y - (height / 2) * zoom
+
+def job_init(args) :
+    global x, y, zoom, width, height, dwell
+    global base_x, base_y, img
+    if len(args) == 6 :
+        x, y, zoom, width, height, dwell = args
+    base_x = x - (width / 2) * zoom
+    base_y = y - (height / 2) * zoom
+    img = Image.new('RGB', (width, height))
+
+def job_get_arg(task_num) : 
+    global width, height, base_x, base_y, zoom, dwell   
+    if (task_num >= width * height) : return ''
+    
+    x = base_x + (task_num % width) * zoom
+    y = base_y + (task_num / width) * zoom
+    return (dwell, x, y)
+    
+def job_add_result(task_num, result) :
+    global width, height, img
+    if (task_num >= width * height) : return
+    #print task_num
+    
+    x = task_num % width
+    y = task_num / width
+    
+    img.putpixel((x, y), result)
+  
+def job_finish() :
+    img.save('fractal.png')
+
+def job_worker(arg) :
+    dwell, x, y = arg
+    import cmath
+    
+    c = complex(x, y)
+    z = complex(0, 0)
+    
+    iter = 0
+    while iter < dwell :
+        z = z * z + c
+        if (z.conjugate() * z).real > 4.0 :
+            break
+        iter = iter + 1
+        
+    if iter == dwell :
+        result = (0, 0, 0)
+    else : 
+        result = (255 - iter % 256, ((iter >> 4) % 256) * 15, 0)
+    
+    return result
