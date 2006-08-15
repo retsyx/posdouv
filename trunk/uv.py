d12297d5fd47166d357ed230c0d050d5c46a7b32
xyster <xyster@4caef4ad-2c1a-0410-9467-39dbfd329abf>
2006-08-15 06:40:46 +0000

    Add auto-scaling of workload depending on how fast a UV is - aka UV power. Illustrates quite well how the Mandelbrot example job doesn't scale overly well because the network bandwidth required to move task args and results becomes rather large as UV power increases.
    UV can also be run on a Power PC...

--- trunk/uv.py
+++ trunk/uv.py
@@ -1,11 +1,17 @@
 import socket
 import sys
 
-dbg_lvl = 1
+dbg_lvl = 3
 
-def dbg(s) :
-        global dbg_lvl
-        if dbg_lvl > 3 : print ''.join(s) 
+def dbg_out(lvl, s) :
+    global dbg_lvl
+    if lvl <= dbg_lvl : print ''.join([str(x) for x in s])
+
+def dmp(s) : dbg_out(5, s)
+def dbg(s) : dbg_out(4, s)    
+def info(s) : dbg_out(3, s)
+def wrn(s) : dbg_out(2, s)
+def err(s) : dbg_out(1, s)
 
 def so_read_line(so) :
     s = ''
@@ -20,30 +26,38 @@ def so_read_line(so) :
     return s
     
 def so_read_block(so) :
-	# read length
-	s = so_read_line(so)
-	try :
-		lblk = long(s)
-	except :
-		lblk = 0
-	
-	# if nothing to read, quit
-	if lblk == 0 : return ''
+    # read length
+    s = so_read_line(so)
+    try :
+        lblk = long(s)
+    except :
+        lblk = 0
+    
+    # if nothing to read, quit
+    if lblk == 0 : return ''
 
- 	# read payload
-	blk = so.recv(lblk)
-	dbg(('=>', s, '\n', blk))
-	return blk
+    # read payload
+    blk = []
+    blk_read = 0
+    while blk_read < lblk :
+        part = so.recv(lblk)
+        blk.append(part)
+        blk_read = blk_read + len(part)
+        
+    blk = ''.join(blk)
+    dmp(('=>', s, '\n', blk))
+    return blk
+
 
 def so_read_task(so) :
     s = so_read_block(so)
-    task_info, result = s.split('\n', 1)
-    return eval(task_info), result
+    task_info, task = s.split('\n', 1)
+    return eval(task_info), task
 
 def so_write_block(so, s) :
-	t = str(len(s)) + '\n' + s
-	dbg(('<=', t))
-	so.send(t)
+    t = str(len(s)) + '\n' + s
+    dmp(('<=', t))
+    so.send(t)
 
 def so_write_task(so, s) :
     task_info, s = s
@@ -56,21 +70,28 @@ port = long(sys.argv[2])
 so = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 so.connect((host, port))
 while 1 :
-	task_info, task = so_read_task(so)
-	
-	if task == '' : break
-	
-	# clear result
-	result = ''
- 	
-	# execute payload
-	#try :
-	exec(task)
-	#except :
-    # print "Failed to run task"
-		
-	# return result
-	so_write_task(so, (task_info, str(result)))
+    task_info, task = so_read_task(so)
+    
+    if task == '' : break
+    
+    task_args = task_info[1]
+    dmp((task_args))
+    
+    task_results = []
+    
+    for arg in task_args :
+        dmp(('arg = ', arg))
+       
+        # clear result
+        result = ''
+            
+        # execute payload
+        exec(task)
+        
+        task_results.append(result)
+        
+    # return result
+    so_write_task(so, ((task_info[0], task_results), ''))
 
 so.close()
  
