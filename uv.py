ce5ab53a17f75a1fcb24d52ae48eb477b4458404
xyster <xyster@4caef4ad-2c1a-0410-9467-39dbfd329abf>
2006-10-09 01:02:14 +0000

    Push UV main loop into uv_run(). Create registry and associated loading/saving (probably still need to add periodic saving). Prove registry as global to the running task. Use optparse for flexibility.

--- uv.py
+++ uv.py
@@ -1,3 +1,6 @@
+from optparse import OptionParser, make_option
+#import optparse
+import pickle
 import socket
 import sys
 import time
@@ -78,61 +81,99 @@ def so_write_task(so, s) :
     task_info, s = s
     s = str(task_info) + '\n' + s
     so_write_block(so, s)
- 
-host = sys.argv[1]
-port = long(sys.argv[2])
 
-job_globals = ''
+REGISTRY_FILE = 'uv.reg'
 
-while 1 :
+# XXX for now just assume pwd is the right place for the registry file
+def reg_save(registry) :
     try :
-        so = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        so.connect((host, port))
-        while 1 :
-            task_info, task = so_read_task(so)
-            task_base, task_globals, task_args = task_info
-        
-            # only compile task code if it is given, otherwise
-            # use previous code
-            if task != '' :
-                dbg(('compiling new task code'))
-                task_str = task[:] # keep a copy for reference if needed later
-                task_obj = compile(task, 'posdo', 'exec')
-                # execute payload
-                task_inst = struct()
-                exec(task_obj, task_inst.__dict__)
-                task_inst.__dict__.update({'uv_ver': uv_ver}) # to support upgrade
-                task_inst.__dict__.update({'so': so}) # XXX to support upgrade
-            
-            # only evaluate globals if given
-            if task_globals != '' :
-                dbg(('assigning new globals'))
-                dmp((task_globals))
-                job_globals = task_globals
-                task_inst.__dict__.update({'job_globals': job_globals})
-                
-            dmp((task_args))
-            
-            # Interpret zero length args as signal to exit
-            if len(task_args) == 0 : break
-            
-            task_results = []
-            
-            for arg in task_args :
-                dmp(('arg = ', arg))
-               
-                result = task_inst.job_worker(arg)
-                
-                task_results.append(result)
-                
-            # return result
-            so_write_task(so, ((task_base, task_results), ''))
-    except Exception, inst: 
-        print 'Exception:', inst
-        so.close()
-    
-    # sleep a little before hammering the server
-    time.sleep(random.randint(1, 10))
+        pickle.dump(registry, open(REGISTRY_FILE, 'w'))    
+    except Exception, inst:
+        err(("Failed to save registry file '", REGISTRY_FILE, "': ", inst, "\nRegistry dump follows:"))
+        try :        
+            err((pickle.dumps(registry)))
+        except Exception, inst:
+            err(("Failed to dump registry: ", inst))
 
-so.close()
+def reg_load() :
+    try :
+        return pickle.load(open(REGISTRY_FILE, 'r'))
+    except Exception, inst:
+        err(("Failed to load registry file '", REGISTRY_FILE, "': ", inst))
+        return {}
+
+def uv_run(registry) :
+    job_globals = ''
+    while 1 :
+        try :
+            so = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            so.connect((host, port))
+            while 1 :
+                task_info, task = so_read_task(so)
+                task_base, task_globals, task_args = task_info
+        
+                # only compile task code if it is given, otherwise
+                # use previous code
+                if task != '' :
+                    dbg(('compiling new task code'))
+                    task_str = task[:] # keep a copy for reference if needed later
+                    task_obj = compile(task, 'posdo', 'exec')
+                    # execute payload
+                    task_inst = struct()
+                    exec(task_obj, task_inst.__dict__)
+                    task_inst.__dict__.update({'uv_registry': uv_registry}) # registry
+                    task_inst.__dict__.update({'uv_ver': uv_ver}) # to support upgrade
+                    task_inst.__dict__.update({'so': so}) # XXX to support upgrade
+                
+                # only evaluate globals if given
+                if task_globals != '' :
+                    dbg(('assigning new globals'))
+                    dmp((task_globals))
+                    job_globals = task_globals
+                    task_inst.__dict__.update({'job_globals': job_globals})
+                
+                dmp((task_args))
+                
+                # Interpret zero length args as signal to exit
+                if len(task_args) == 0 : break
+                
+                task_results = []
+                
+                for arg in task_args :
+                    dmp(('arg = ', arg))
+                
+                    result = task_inst.job_worker(arg)
+                    
+                    task_results.append(result)
+                
+                # return result
+                so_write_task(so, ((task_base, task_results), ''))
+        except Exception, inst: 
+            err(('Exception: ', inst))
+            so.close()
+    
+        # sleep a little before hammering the server
+        time.sleep(random.randint(1, 10))
+    
+    so.close()
+
+option_list = [
+make_option('-a', '--addr', type='string', default='localhost', dest='host_addr'),
+make_option('-p', '--port', type='int', default=6666, dest='host_port'),
+]
+
+parser = OptionParser(option_list=option_list)
+(options, args) = parser.parse_args()
+
+host = options.host_addr
+port = options.host_port
+
+registry = reg_load()
+
+try :
+    uv_run(registry)
+except Exception, inst :
+    err(('Exception: ', inst))
+
+reg_save(registry)
  
