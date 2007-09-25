3ebc2844b8ef95245b9eb02bc8249491e84c38af
xyster <xyster@4caef4ad-2c1a-0410-9467-39dbfd329abf>
2007-09-25 03:19:04 +0000

    jobs.py to display number of running jobs. PosdoAccessor modified to facilitate above.

---
+++ trunk/jobs.py
@@ -0,0 +1,16 @@
+# jobs.py
+# display running jobs
+def job_init(args):  return 0
+
+def job_get_globals(): return ''
+
+def job_get_arg(task_num): return None
+    
+def job_add_result(task_num, result): pass
+
+def job_notify_failure(task_num): pass
+
+def job_finish():
+ posdo.info('Posdo has %d jobs' % (posdo.jobs_nof()-1))
+       
+def job_worker(arg): pass
