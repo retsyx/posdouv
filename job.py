cb403ba4be2244cce12549137cd052730a9d272d
xyster <xyster@4caef4ad-2c1a-0410-9467-39dbfd329abf>
2007-12-17 03:09:44 +0000

    Add job_get_status() to allow finer grained control of task dispatching. Allows a job to not provide args without implicitly signalling that the job is complete.

--- job.py
+++ job.py
@@ -4,6 +4,10 @@
 i = 0
 
 def job_init(args):
+    """
+        Initialize the job.
+        Returns 0 on success.
+    """
     global i
     i = 0
     posdo.info('running with %d uvs' % (posdo.uvs_nof()))
@@ -12,9 +16,28 @@ def job_init(args):
 #def job_get_options(): return ...
 
 def job_get_globals():
+    """
+        Get job globals to be used on all UVs.
+        Return empty string to signify no globals.
+    """
     return ''
 
+def job_get_status():
+    """ Obtain job status (optional).
+        Returns:
+        0, 0 - job complete
+        1, N - job not complete, require N more results before job_get_arg() can be called
+    """
+    global i
+    if i == 10: return 0, 0
+    return 1, 0
+    
 def job_get_arg(task_num):
+    """
+        Get a job argument/task to be executed on a UV.
+        Returns None to signify no more arguments/tasks are left
+        and that this job is done (optional if job_get_status() is defined).
+    """
     global i 
     if i == 10: return None # return None to signal the end of the job
     arg = str(i)
@@ -22,11 +45,21 @@ def job_get_arg(task_num):
     return arg
 
 def job_add_result(task_num, result):
+    """
+        Process a result of an executed argument/task
+    """    
     posdo.info(result)
 
-def job_notify_failure(task_num): pass
+def job_notify_failure(task_num): 
+    """
+        Process an argument/task execution failure (optional)
+    """
+    pass
 
 def job_finish():
+    """
+        Called when all job processing is complete.
+    """    
     posdo.info('job completed')
 
 # Posdo will remote job_worker() and below to UV
