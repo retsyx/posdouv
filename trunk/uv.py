e73e153b95c9c7eec765219938909e20a18f7240
xyster <xyster@4caef4ad-2c1a-0410-9467-39dbfd329abf>
2007-12-18 03:57:32 +0000

    Use pickle instead of str()/eval() to move around task arguments and code.

--- trunk/uv.py
+++ trunk/uv.py
@@ -55,8 +55,8 @@ def so_read_block(so):
     return s
 def so_read_task(so):
     s = so_read_block(so)
-    task_info, task = s.split('\n', 1)
-    return eval(task_info), task
+    task_info, task = pickle.loads(s)
+    return task_info, task
 def so_write_block(so, r):
     dmp('<=', len(r), r)
     s = zlib.compress(r)
@@ -64,7 +64,7 @@ def so_write_block(so, r):
     so.sendall(t)
 def so_write_task(so, s):
     task_info, s = s
-    s = str(task_info) + '\n' + s
+    s = pickle.dumps((task_info, s))
     so_write_block(so, s)
 
 REGISTRY_SAVE_INTERVAL = 600
